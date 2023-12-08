0067248f30305da82df6e7b04b92f068c8ad6f9e
USER <msobanakram7@gmail.com>
2023-12-08 16:39:32 +0500

    After implementing desktop design

---
+++ script/script.js
@@ -0,0 +1,33 @@
+function showMessage(event) {
+    // prevent the default form submission
+    event.preventDefault();
+    // hide the first page
+    document.getElementsByClassName('container')[0].style.display = 'none';
+    // show confirmation page;
+    document.getElementsByClassName('confirmationMessage')[0].style.display = 'flex';
+}
+
+document.addEventListener('DOMContentLoaded', function()
+{
+    let msg = document.getElementById('dismissBtn');
+    msg.addEventListener('click', function(event){
+        document.getElementsByClassName('confirmationMessage')[0].style.display = 'none';
+        let page = document.getElementsByClassName('container')[0];
+        page.style.display = 'flex';
+    });
+    let userEmail = document.getElementById('userEmail');
+    userEmail.addEventListener('blur', function(){
+        if (!userEmail.validity.valid)
+        {
+            document.getElementById('errorMsg').style.display = 'block';
+        }
+        else
+        {
+            document.getElementById('errorMsg').style.display = 'none';
+        }
+    });
+});
+
+
+
+
